e5fe8b1389b65f689cdb448e4805f5fb4220f3c9
harsh <harshnebhnani02@gmail.com>
2026-01-24 15:05:36 +0530

    changed api

--- api/health.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ success: false, message: 'Method not allowed' });
-  }
-  
-  res.json({ status: 'ok', message: 'Server is running' });
-}
